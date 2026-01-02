d66c25b395027d3e26fae42f61bbdd6925c91362
Paul Landes <landes@mailc.net>
2026-01-02 16:56:37 -0600

    add invocation test

--- test/choice-program-test.el
+++ test/choice-program-test.el
@@ -21,31 +21,46 @@
 		  :interpreter "/bin/sh"
 		  :buffer-name "*Synchronized Output*"
 		  :choice-prompt "Mnemonic"
-		  :choice-switch-name "-m"
+		  :choice-switch-name ""
 		  :selection-args '("-a" "listmnemonics")
 		  :documentation
 "Run a synchronize command.  The command is issued with the `synconf'
 perl script.")
   "The synconf object instance.")
 
-(choice-program-create-exec-function
- 'synconf-the-instance)
+(defun choice-program-test-setup ()
+  "Initialize the test."
+  (choice-program-create-exec-function 'synconf-the-instance)
+  (let ((dir (concat (file-name-as-directory
+		      (expand-file-name default-directory))
+		     "test")))
+    (add-to-list 'exec-path dir)))
 
-(let ((dir (concat (file-name-as-directory
-		    (expand-file-name default-directory)) "test")))
-  (add-to-list 'exec-path dir))
-
-(ert-deftest test-choice-program-selections ()
+(ert-deftest choice-program-test-selections ()
   "Validate mnemonic options."
+  (choice-program-test-setup)
   (->> synconf-the-instance
        (choice-program-selections)
        (equal '("laptop" "usb"))
        should))
 
-(ert-deftest test-choice-prompt ()
+(ert-deftest choice-program-test-prompt ()
   "Validate prompt."
+  (choice-program-test-setup)
   (should (equal "Test (default def): "
 		 (choice-program-complete-default-prompt "Test" "def"))))
+
+(ert-deftest choice-program-invoke ()
+  "Validate prompt."
+  (choice-program-test-setup)
+  (let ((this synconf-the-instance))
+    (choice-program-exec this "-a listmnemonics")
+    (sit-for 0.2)
+    (let ((output (with-current-buffer
+		      (get-buffer (slot-value this 'buffer-name))
+		    (buffer-substring-no-properties (point-min) (point-max)))))
+      (should (string-search "laptop" output))
+      (should (string-search "usb" output)))))
 
 (provide 'choice-program-test)
 
